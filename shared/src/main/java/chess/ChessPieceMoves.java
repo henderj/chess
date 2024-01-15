1f47aeb8cb85a996bd8f5b16b200fcd9b32539e9
Josh Hendershot <hendershotjoshr@gmail.com>
2024-01-15 16:48:11 -0700

    implement rest of pawn moves

--- shared/src/main/java/chess/ChessPieceMoves.java
+++ shared/src/main/java/chess/ChessPieceMoves.java
@@ -25,82 +25,190 @@ public class ChessPieceMoves {
 
         for (int i = 1; i <= distance; i++) {
             var NE = new ChessPosition(position.getRow() + i, position.getColumn() + i);
-            if (addMoveIfValid(board, position, color, moves, NE)) break;
+            var move = new ChessMove(position, NE);
+            if (!isValid(board, move, color)) break;
+            moves.add(move);
+            if (isCapture(board, move, color)) break;
         }
 
         for (int i = 1; i <= distance; i++) {
             var SE = new ChessPosition(position.getRow() - i, position.getColumn() + i);
-            if (addMoveIfValid(board, position, color, moves, SE)) break;
+            var move = new ChessMove(position, SE);
+            if (!isValid(board, move, color)) break;
+            moves.add(move);
+            if (isCapture(board, move, color)) break;
         }
 
         for (int i = 1; i <= distance; i++) {
             var SW = new ChessPosition(position.getRow() - i, position.getColumn() - i);
-            if (addMoveIfValid(board, position, color, moves, SW)) break;
+            var move = new ChessMove(position, SW);
+            if (!isValid(board, move, color)) break;
+            moves.add(move);
+            if (isCapture(board, move, color)) break;
         }
 
         for (int i = 1; i <= distance; i++) {
             var NW = new ChessPosition(position.getRow() + i, position.getColumn() - i);
-            if (addMoveIfValid(board, position, color, moves, NW)) break;
-        }
-
-        return moves;
-    }
-
-    public static Collection<ChessMove> straightMoves(ChessBoard board, ChessPosition position, ChessGame.TeamColor color){
-        return straightMoves(board, position, color, 7);
-    }
-
-    public static Collection<ChessMove> straightMoves(ChessBoard board, ChessPosition position,
-                                                      ChessGame.TeamColor color, int distance) {
-        HashSet<ChessMove> moves = new HashSet<>();
-
-        for (int i = 1; i <= distance; i++) {
-            var N = new ChessPosition(position.getRow() + i, position.getColumn());
-            if (addMoveIfValid(board, position, color, moves, N)) break;
-        }
-
-        for (int i = 1; i <= distance; i++) {
-            var E = new ChessPosition(position.getRow(), position.getColumn() + i);
-            if (addMoveIfValid(board, position, color, moves, E)) break;
-        }
-
-        for (int i = 1; i <= distance; i++) {
-            var S = new ChessPosition(position.getRow() - i, position.getColumn());
-            if (addMoveIfValid(board, position, color, moves, S)) break;
-        }
-
-        for (int i = 1; i <= distance; i++) {
-            var W = new ChessPosition(position.getRow(), position.getColumn() - i);
-            if (addMoveIfValid(board, position, color, moves, W)) break;
+            var move = new ChessMove(position, NW);
+            if (!isValid(board, move, color)) break;
+            moves.add(move);
+            if (isCapture(board, move, color)) break;
         }
 
         return moves;
     }
 
     /**
-     * Checks if the given end position is a valid position for the piece to move to. If so, adds it to
-     * <code>moves</code>
+     * Calculates all the straight moves available to a chess piece up to the edge of the board
      *
-     * @return <code>true</code> if the piece can't move further. Otherwise, <code>false</code>
+     * @return Collection of straight moves
      */
-    private static boolean addMoveIfValid(ChessBoard board, ChessPosition position, ChessGame.TeamColor color,
-                                          HashSet<ChessMove> moves, ChessPosition endPosition) {
-        if (!board.inBounds(endPosition)) return true;
-        var piece = board.getPiece(endPosition);
-        if (piece != null && piece.getTeamColor().equals(color)) return true; // can't capture own team
-        moves.add(new ChessMove(position, endPosition));
-        return piece != null; // can capture other team, but can't move further
+    public static Collection<ChessMove> straightMoves(ChessBoard board, ChessPosition position,
+                                                      ChessGame.TeamColor color) {
+        return straightMoves(board, position, color, 7);
     }
 
+    /**
+     * Calculates all the straight moves available to a chess piece up to <code>distance</code> squares
+     *
+     * @return Collection of straight moves
+     */
+    public static Collection<ChessMove> straightMoves(ChessBoard board, ChessPosition position,
+                                                      ChessGame.TeamColor color, int distance) {
+        HashSet<ChessMove> moves = new HashSet<>();
+
+        for (int i = 1; i <= distance; i++) {
+            var N = new ChessPosition(position.getRow() + i, position.getColumn());
+            var move = new ChessMove(position, N);
+            if (!isValid(board, move, color)) break;
+            moves.add(move);
+            if (isCapture(board, move, color)) break;
+        }
+
+        for (int i = 1; i <= distance; i++) {
+            var E = new ChessPosition(position.getRow(), position.getColumn() + i);
+            var move = new ChessMove(position, E);
+            if (!isValid(board, move, color)) break;
+            moves.add(move);
+            if (isCapture(board, move, color)) break;
+        }
+
+        for (int i = 1; i <= distance; i++) {
+            var S = new ChessPosition(position.getRow() - i, position.getColumn());
+            var move = new ChessMove(position, S);
+            if (!isValid(board, move, color)) break;
+            moves.add(move);
+            if (isCapture(board, move, color)) break;
+        }
+
+        for (int i = 1; i <= distance; i++) {
+            var W = new ChessPosition(position.getRow(), position.getColumn() - i);
+            var move = new ChessMove(position, W);
+            if (!isValid(board, move, color)) break;
+            moves.add(move);
+            if (isCapture(board, move, color)) break;
+        }
+
+        return moves;
+    }
+
+    /**
+     * Calculates the pawn moves for a given position on the board
+     *
+     * @return Collection of pawn moves
+     */
     public static Collection<ChessMove> pawnMoves(ChessBoard board, ChessPosition position, ChessGame.TeamColor color) {
         HashSet<ChessMove> moves = new HashSet<>();
-        if (color == ChessGame.TeamColor.WHITE){
-            var forward = new ChessPosition(position.getRow() + 1, position.getColumn());
-            addMoveIfValid(board, position, color, moves, forward);
-        } else {
-            var forward = new ChessPosition(position.getRow() - 1, position.getColumn());
-            addMoveIfValid(board, position, color, moves, forward);
+        var row = position.getRow();
+        var col = position.getColumn();
+        var forward = 1;
+        var startingRow = 2;
+        var endRow = 8;
+        if (color == ChessGame.TeamColor.BLACK) {
+            forward = -1;
+            startingRow = 7;
+            endRow = 1;
         }
+        var forwardMove = new ChessMove(position, new ChessPosition(row + forward, col));
+        if (isValid(board, forwardMove, color, false)) {
+            if (forwardMove.getEndPosition().getRow() == endRow) {
+                moves.addAll(getPromotionMoves(forwardMove));
+            } else {
+                moves.add(forwardMove);
+            }
+            if (row == startingRow) {
+                var doubleForwardMove = new ChessMove(position, new ChessPosition(row + forward * 2, col));
+                if (isValid(board, doubleForwardMove, color, false)) moves.add(doubleForwardMove);
+            }
+        }
+
+        var diagonalCaptureLeft = new ChessMove(position, new ChessPosition(row + forward, col - 1));
+        if (isValid(board, diagonalCaptureLeft, color) && isCapture(board, diagonalCaptureLeft, color)) {
+            if (diagonalCaptureLeft.getEndPosition().getRow() == endRow) {
+                moves.addAll(getPromotionMoves(diagonalCaptureLeft));
+            } else {
+                moves.add(diagonalCaptureLeft);
+            }
+        }
+
+        var diagonalCaptureRight = new ChessMove(position, new ChessPosition(row + forward, col + 1));
+        if (isValid(board, diagonalCaptureRight, color) && isCapture(board, diagonalCaptureRight, color)) {
+            if (diagonalCaptureRight.getEndPosition().getRow() == endRow) {
+                moves.addAll(getPromotionMoves(diagonalCaptureRight));
+            } else {
+                moves.add(diagonalCaptureRight);
+            }
+        }
+
+        return moves;
+    }
+
+    /**
+     * Checks if the given move is a valid move for the piece
+     *
+     * @param canCapture if the piece can capture with this move
+     * @return <code>true</code> if the piece can move further there. Otherwise, <code>false</code>
+     */
+    private static boolean isValid(ChessBoard board, ChessMove move, ChessGame.TeamColor color, boolean canCapture) {
+        if (!board.inBounds(move.getEndPosition())) return false;
+        var piece = board.getPiece(move.getEndPosition());
+        if (piece == null) return true;
+        if (piece.getTeamColor().equals(color)) return false; // can't capture own team
+        return !piece.getTeamColor().equals(color) && canCapture;
+    }
+
+    /**
+     * Checks if the given move is a valid move for the piece. Assumes the piece can capture
+     * enemy pieces at the end position.
+     *
+     * @return <code>true</code> if the move is valid. Otherwise, <code>false</code>
+     */
+    private static boolean isValid(ChessBoard board, ChessMove move, ChessGame.TeamColor color) {
+        return isValid(board, move, color, true);
+    }
+
+    /**
+     * Checks if a given <code>ChessMove</code> is a capture by checking if there is a piece at the end position
+     * and if that piece is the opposite color
+     *
+     * @return <code>true</code> if the move is a capture. Otherwise, <code>false</code>
+     */
+    private static boolean isCapture(ChessBoard board, ChessMove move, ChessGame.TeamColor color) {
+        var piece = board.getPiece(move.getEndPosition());
+        return piece != null && piece.getTeamColor() != color;
+    }
+
+    /**
+     * Constructs the promotions moves for a given moves. It adds promotions to Bishop, Knight, Rook, and Queen
+     *
+     * @return Collection of promotion moves
+     */
+    private static Collection<ChessMove> getPromotionMoves(ChessMove move) {
+        var moves = new HashSet<ChessMove>();
+        moves.add(new ChessMove(move.getStartPosition(), move.getEndPosition(), ChessPiece.PieceType.BISHOP));
+        moves.add(new ChessMove(move.getStartPosition(), move.getEndPosition(), ChessPiece.PieceType.KNIGHT));
+        moves.add(new ChessMove(move.getStartPosition(), move.getEndPosition(), ChessPiece.PieceType.ROOK));
+        moves.add(new ChessMove(move.getStartPosition(), move.getEndPosition(), ChessPiece.PieceType.QUEEN));
         return moves;
     }
 }
