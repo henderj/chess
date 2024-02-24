735c58af6eee5e1a199658fb3cd69458443c7837
Josh Hendershot <hendershotjoshr@gmail.com>
2024-02-24 09:12:53 -0700

    more formatting MemoryAuthDAO

--- server/src/test/java/dataAccessTests/UserDAOTests.java
+++ server/src/test/java/dataAccessTests/UserDAOTests.java
@@ -19,46 +19,31 @@ public class UserDAOTests {
     @MethodSource("implementations")
     public void cantInsertUserTwice(UserDAO userDAO) {
         UserData user = new UserData("name", "password", "email@email.com");
-        Assertions.assertDoesNotThrow(() -> {
-            userDAO.insertUser(user);
-        });
-
-        Assertions.assertThrows(DataAccessException.class, () -> {
-            userDAO.insertUser(user);
-        });
+        Assertions.assertDoesNotThrow(() -> userDAO.insertUser(user));
+        Assertions.assertThrows(DataAccessException.class, () -> userDAO.insertUser(user));
     }
 
     @ParameterizedTest
     @MethodSource("implementations")
     public void cantInsertUserWithSameUsername(UserDAO userDAO) {
         UserData user = new UserData("name", "password", "email@email.com");
-        Assertions.assertDoesNotThrow(() -> {
-            userDAO.insertUser(user);
-        });
+        Assertions.assertDoesNotThrow(() -> userDAO.insertUser(user));
         UserData user2 = new UserData("name", "password2", "email2@email.com");
-        Assertions.assertThrows(DataAccessException.class, () -> {
-            userDAO.insertUser(user2);
-        });
+        Assertions.assertThrows(DataAccessException.class, () -> userDAO.insertUser(user2));
     }
 
     @ParameterizedTest
     @MethodSource("implementations")
     public void canCreateAndGetUser(UserDAO userDAO) {
         UserData user = new UserData("name", "password", "email@email.com");
-        Assertions.assertDoesNotThrow(() -> {
-            userDAO.insertUser(user);
-        });
-        Assertions.assertDoesNotThrow(() -> {
-          Assertions.assertEquals(user, userDAO.readUser(user.username()));
-        });
+        Assertions.assertDoesNotThrow(() -> userDAO.insertUser(user));
+        Assertions.assertDoesNotThrow(() -> Assertions.assertEquals(user, userDAO.readUser(user.username())));
     }
 
     @ParameterizedTest
     @MethodSource("implementations")
     public void getNonexistentUserReturnsNull(UserDAO userDAO) {
-       Assertions.assertDoesNotThrow(() -> {
-           Assertions.assertNull(userDAO.readUser("not_a_name"));
-       });
+       Assertions.assertDoesNotThrow(() -> Assertions.assertNull(userDAO.readUser("not_a_name")));
     }
 
     @ParameterizedTest
@@ -67,9 +52,7 @@ public class UserDAOTests {
         UserData user = new UserData("name", "password", "email@email.com");
         Assertions.assertDoesNotThrow(() -> userDAO.insertUser(user));
         userDAO.clear();
-        Assertions.assertDoesNotThrow(() -> {
-            Assertions.assertNull(userDAO.readUser(user.username()));
-        });
+        Assertions.assertDoesNotThrow(() -> Assertions.assertNull(userDAO.readUser(user.username())));
     }
 
 }
