9990504f3a1a4f36de79de790428415e6fa3139f
Josh Hendershot <hendershotjoshr@gmail.com>
2024-03-18 17:29:15 -0600

    server facade skeleton

--- shared/src/main/java/serverFacade/ServerFacade.java
+++ shared/src/main/java/serverFacade/ServerFacade.java
@@ -1,17 +1,38 @@
 package serverFacade;
 
 
-import schema.request.LoginRequest;
-import schema.request.RegisterRequest;
-import schema.response.LoginResponse;
-import schema.response.RegisterResponse;
+import model.GameData;
+import schema.request.*;
+import schema.response.*;
 
 public class ServerFacade {
     public RegisterResponse register(RegisterRequest request) {
+        // TODO
         return new RegisterResponse("TODO", "TODO");
     }
 
     public LoginResponse login(LoginRequest request) {
+        // TODO
         return new LoginResponse("TODO", "TODO");
+    }
+
+    public LogoutResponse logout(LogoutRequest request) {
+        // TODO
+        return new LogoutResponse();
+    }
+
+    public CreateGameResponse createGame(CreateGameRequest request) {
+        // TODO
+        return new CreateGameResponse(1234);
+    }
+
+    public ListGamesResponse listGames(ListGamesRequest request) {
+        // TODO
+        return new ListGamesResponse(new GameData[]{new GameData(1234, null, null, "TODO", null)});
+    }
+
+    public JoinGameResponse joinGame(JoinGameRequest request) {
+        // TODO
+        return new JoinGameResponse();
     }
 }
