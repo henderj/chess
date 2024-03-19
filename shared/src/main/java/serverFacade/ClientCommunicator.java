b1ef4b6de9eb4bb748103ace4ac2f760a209c042
Josh Hendershot <hendershotjoshr@gmail.com>
2024-03-18 20:16:28 -0600

    client communicator initial code

--- shared/src/main/java/serverFacade/ClientCommunicator.java
+++ shared/src/main/java/serverFacade/ClientCommunicator.java
@@ -1,4 +1,71 @@
 package serverFacade;
 
+import com.google.gson.Gson;
+import exception.ResponseException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
 public class ClientCommunicator {
+    private final String serverUrl;
+
+    public ClientCommunicator(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+    public <T> T makeRequest(String method, String path, Object body, Class<T> responseClass) throws ResponseException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(body, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    private static void writeBody(Object body, HttpURLConnection http) throws IOException {
+        if (body != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(body);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private static void throwIfNotSuccessful(HttpURLConnection http) throws ResponseException, IOException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            throw new ResponseException(status, "failure: " + status);
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+    private static boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
 }
