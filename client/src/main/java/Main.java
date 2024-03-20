016a4b745c1d1d0cc46be9d1e9e2c36f56ce8dd4
Josh Hendershot <hendershotjoshr@gmail.com>
2024-03-19 19:48:28 -0600

    start client from main

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,11 @@
 import chess.*;
+import serverFacade.ClientCommunicator;
+import serverFacade.ServerFacade;
+import ui.MenuUI;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        var menu = new MenuUI(new ServerFacade(new ClientCommunicator("http://localhost:8080")));
+        menu.startMenuLoop();
     }
 }
